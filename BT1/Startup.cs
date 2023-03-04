2130f2b09c62aa59ad4282ce44ae13beaba56b19
dendekun <111942541+dendekun@users.noreply.github.com>
2023-03-04 10:48:21 +0700

    Add project files.

---
+++ BT1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BT1.Startup))]
+namespace BT1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
