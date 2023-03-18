3982c09e50dfdaf0a527f5099fe5fe4fb648a66f
dendekun <111942541+dendekun@users.noreply.github.com>
2023-03-18 11:06:26 +0700

    update_03

---
+++ BT1/Controllers/AttendancesController.cs
@@ -0,0 +1,35 @@
+﻿using BT1.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace BT1.Controllers
+{
+    [System.Web.Http.Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _dbContext;
+        public AttendancesController() 
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        [System.Web.Http.HttpPost]
+        public IHttpActionResult Attend([FromBody] int courseId)
+        {
+            var attendance = new Attendance
+            {
+                CourseId = courseId,
+                AttendeeId = User.Identity.GetUserId()
+            };
+            _dbContext.Attendances.Add(attendance);
+            _dbContext.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
