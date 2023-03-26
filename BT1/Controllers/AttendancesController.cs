d7f49e1dca4db2d85fad6a2464e091ed274b7ee1
dendekun <111942541+dendekun@users.noreply.github.com>
2023-03-27 00:42:22 +0700

    update_04

--- BT1/Controllers/AttendancesController.cs
+++ BT1/Controllers/AttendancesController.cs
@@ -1,4 +1,5 @@
-﻿using BT1.Models;
+﻿using BT1.DTOs;
+using BT1.Models;
 using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
@@ -19,12 +20,16 @@ namespace BT1.Controllers
             _dbContext = new ApplicationDbContext();
         }
         [System.Web.Http.HttpPost]
-        public IHttpActionResult Attend([FromBody] int courseId)
+        public IHttpActionResult Attend(AttendanceDto   attendanceDto)
         {
+            var userId = User.Identity.GetUserId();
+            if (_dbContext.Attendances.Any(a => a.AttendeeId == userId && a.CourseId == attendanceDto.CourseId))
+                return BadRequest("The Attendance already exists!");
+
             var attendance = new Attendance
             {
-                CourseId = courseId,
-                AttendeeId = User.Identity.GetUserId()
+                CourseId = attendanceDto.CourseId,
+                AttendeeId = userId
             };
             _dbContext.Attendances.Add(attendance);
             _dbContext.SaveChanges();
